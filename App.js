a548e44f2f25ac2d22f120283a9feddc74328a4f
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-07-30 09:48:11 +0200

    Hook up Redux into the app

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { StyleSheet, Text, View, Slider } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   state = {
@@ -8,9 +11,11 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <AddEntry />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
